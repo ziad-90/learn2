2325b58e10742716eca067de629f79c03e729352
Zeyad Mahmoud Abdelgawad <150368553+ziad-90@users.noreply.github.com>
2025-11-11 20:23:44 +0200

    Create Product.js model file with product schema

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    expiryDate: {
+        type: Date,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
